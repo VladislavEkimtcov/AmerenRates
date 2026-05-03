1874ca4725a06efb1ed338e6a9304c8219c51710
ekimtco2 <ekimtco2@illinois.edu>
2026-05-03 10:01:46 -0500

    testing countergravity

--- 3cols_graph.sh
+++ 3cols_graph.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Gracefully handle termination to avoid hanging processes
+trap "exit 0" SIGINT SIGTERM
+
 # Run 3cols.py every 5 seconds forever
 while true; do
-    python3 3cols_combo.py -bar 7
+    clear
+    nice python3 3cols_combo.py -bar 7
     sleep 5
 done
